500c4578cf6831c97a1c4cbcb3e7b49e36763c47
ArjuSuresh <sureshandarjun@gmail.com>
2024-08-12 10:35:13 +0530

    Add files via upload

---
+++ S3/DS/bubblesort.c
@@ -0,0 +1,52 @@
+#include<stdbool.h>
+#include<stdio.h>
+void swap(int*xp,int*yp)
+{
+ int temp=*xp;
+ *xp=*yp;
+ *yp=temp;
+}
+
+void bubbleSort(int arr[],int n)
+{
+  int i,j;
+  bool swapped;
+  for(i=0;i<n-1;i++)
+  {
+   swapped=false;
+    for(j=0;j<n-i-1;j++)
+    {
+     if(arr[j]>arr[j+1])
+      {
+       swap(&arr[j],&arr[j+1]);
+       swapped=true;
+      }
+    }
+  }
+}
+void printfArray(int arr[],int size)
+{
+ int i;
+ for(i=0;i<size;i++)
+  {
+   printf("%d ",arr[i]);
+  }
+}
+
+int main()
+{
+  int arr[50],i,n;
+  printf("enter no of elements");
+  scanf("%d",&n);
+  printf("enter the array elements:");
+  for(i=0;i<n;i++)
+  {
+   scanf("%d",&arr[i]);
+  }
+  bubbleSort(arr,n);
+  printf("Sorted array:\n");
+  printfArray(arr,n);
+  return 0;
+}
+
+
